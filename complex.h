ed5db8a7a630a2577bb8bc3ce4287899ed6c0689
VedantYadav025 <vedantyadav24725@gmail.com>
2024-07-07 20:52:10 +0530

    added operations for addition and multiplication

--- complex.h
+++ complex.h
@@ -1,10 +1,22 @@
+#include <cmath>
+#include <istream>
 #include <ostream>
+#include <utility>
 template <typename T> class complex {
 public:
   complex();
   complex(const T &real, const T &img);
   template <typename P>
   friend std::ostream &operator<<(std::ostream &output, const complex<P> &num);
+  template <typename P>
+  friend std::istream &operator>>(std::istream &input, const complex<P> &num);
+  complex operator+(const complex &c) const;
+  complex operator-(const complex &c) const;
+  complex conjugate() const;
+  double mod() const;
+  complex operator*(const complex &c) const;
+  complex operator=(const std::pair<T, T> &c);
+  bool operator==(const complex &c) const;
 
 private:
   T real_, img_;
@@ -26,4 +38,52 @@ std::ostream &operator<<(std::ostream &output, const complex<T> &num) {
   else
     output << num.real_ << "-" << num.img_ << "j";
   return output;
+}
+
+template <typename T>
+std::istream &operator>>(std::istream &input, const complex<T> &num) {
+  T real, img;
+  input >> real;
+  input >> img;
+  return img;
+}
+
+template <typename T>
+complex<T> complex<T>::operator+(const complex<T> &c) const {
+  complex<T> add(this->real_ + c.real_, this->img_ + c.img_);
+  return add;
+}
+
+template <typename T>
+complex<T> complex<T>::operator-(const complex<T> &c) const {
+  complex<T> add(this->real_ - c.real_, this->img_ - c.img_);
+  return add;
+}
+
+template <typename T> complex<T> complex<T>::conjugate() const {
+  complex<T> conj(this->real_, -this->img_);
+  return conj;
+}
+
+template <typename T> double complex<T>::mod() const {
+  double mod = std::sqrt(this->real_ * this->real_ + this->img_ * this->img_);
+  return mod;
+}
+
+template <typename T>
+complex<T> complex<T>::operator*(const complex<T> &c) const {
+  T real = this->real_ * c.real_ - this->img_ * c.img_;
+  T img = this->real_ * c.img_ + this->img_ * c.real_;
+  complex<T> mult(real, img);
+  return mult;
+}
+
+template <typename T>
+complex<T> complex<T>::operator=(const std::pair<T, T> &c) {
+  complex<T> num(c.first, c.second);
+  return num;
+}
+
+template <typename T> bool complex<T>::operator==(const complex<T> &c) const {
+  return (this->real_ == c.real_ && this->img_ == c.img_);
 }
